3518360a73f81f1baa453c2c241ee0c5387462d6
Anis Ghabi <anisghabi8@gmail.com>
2023-03-23 14:21:14 +0000

    commit

--- initial.js
+++ initial.js
@@ -3,5 +3,5 @@ const { stringify } = require('querystring');
 
 fs.readFile('test.json', "utf8", (error, data) => {
     jsond =  JSON.parse(data)
-    console.log(jsond['WorkspaceResponse'][0].createdAt);
+    console.log(jsond['WorkspaceResponse'][0]);
 });
